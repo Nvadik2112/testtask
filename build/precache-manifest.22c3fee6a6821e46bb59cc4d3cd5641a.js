e13946ca1bc843eef395b0ca33fd4ba0dd30dfc6
Vadim Nemchenko <nvadik2112@yandex.ru>
2020-03-15 01:00:27 +0300

    final build

---
+++ build/precache-manifest.22c3fee6a6821e46bb59cc4d3cd5641a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d0787377017045314ff3bc2ed9a9ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "471b743f6b35769bf24f",
+    "url": "./static/js/2.50b7c740.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.50b7c740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605822746be9b8be4862",
+    "url": "./static/js/main.56b60057.chunk.js"
+  },
+  {
+    "revision": "567ca5cea940a0c54188",
+    "url": "./static/js/runtime-main.1a632c21.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
